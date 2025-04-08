0b7f56a26c0e1ffb1e207ac6c7c8283451ff3fc6
Jithesh Pavan DSouza <140151086+JitheshPavan@users.noreply.github.com>
2025-04-08 22:30:32 +0530

    Directly added access to deepseek small model

--- src/probing_utils.py
+++ src/probing_utils.py
@@ -304,11 +304,11 @@ def extract_internal_reps_all_layers_and_tokens(model, input_output_ids_lst, pro
 def load_model_and_validate_gpu(model_path, tokenizer_path=None):
     if tokenizer_path is None:
         tokenizer_path = model_path
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
     print("Started loading model")
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto',
-                                                 torch_dtype=torch.bfloat16, low_cpu_mem_usage=True)
+    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", device_map='auto', torch_dtype=torch.bfloat16,low_cpu_mem_usage=True)
     assert ('cpu' not in model.hf_device_map.values())
+    print("model loaded")
     return model, tokenizer
 
 
